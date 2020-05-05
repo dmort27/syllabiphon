9108f3ebdc81f5f1dcdfcbc786aadb15e00e1dc9
David R. Mortensen <dmortens@cs.cmu.edu>
2020-05-05 15:37:23 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='syllabiphon',
+   version='0.1',
+   description='A module for sonority-driven syllabification',
+   author='David R. Mortensen',
+   author_email='dmortens@cs.cmu.edu,
+   packages=['syllabiphon'],
+   install_requires=['panphon'],
+)
